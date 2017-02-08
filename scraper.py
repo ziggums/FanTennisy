ecd6f617c5b366ea76557f603d45e2e883fa7b5f
Houston <Houston@evilgeniustechnologies.com>
2017-02-08 11:23:33 -0500

    First commit

---
+++ scraper.py
@@ -0,0 +1,6 @@
+import urllib2
+import requests
+
+API_URL = 'http://api.espn.com/v1/sports/tennis/atp'
+
+response
---
+++ untitled.py
@@ -0,0 +1,11 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+table = soup.find('tbody', attrs={'class': 'stripe'})
+
+print soup.prettify()

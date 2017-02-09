4f67a143dcab9b56e2a5ded102806197faa9c20f
Houston <Houston@evilgeniustechnologies.com>
2017-02-08 22:24:49 -0500

    Feedback from Brandon, stopped redefining cell in the for loop. Now need to igure out how to isolate the src url for flag images, as well as handle beyond 'tr' to see when a player is advancing

--- scraper.py
+++ scraper.py
@@ -8,29 +8,10 @@ html = response.content
 soup = BeautifulSoup(html)
 table = soup.find(attrs={'class': 'scores-draw-table'})
 
-for row in table.findAll('tr'):
-	print row.prettify()
-
-'''
-For Brandon:
-So the above for statment returns everything I need, but not in the base form.
-However, when I try to run the following:
-
-for row in table.findAll('tr'):
-	for cell in row.findAll('td'):
-		print cell.text
-
-It prints out the names, seeds, etc. but not the stuff within 'td' that is
-<img class
-
-This would be nice since it's the url for the flag of the player, and I could
-use that for my eventual front end.
-
-I suppose I could call those separately, but I'm not sure if there would be an
-easy way to match them up.
-
-Is there a way I can get that URL in line with everything else, to make it nice and neat?
-'''
+for row in table.findAll('tbody'):
+	for cell in row.findAll('tr'):
+		print cell.text # text in cell
+		print cell.find('img') # image in cell
 
 
 

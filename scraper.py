30c23433a1e1e5a48bcc0f43b60c11ab6e1ac195
Houston <Houston@evilgeniustechnologies.com>
2017-02-08 21:21:34 -0500

    Switched to Draw URL rather than results URL, for more logical creation of a bracket. Have parsed down to 'tr', but am having trouble getting to 'td' while still returning the URL within <img class for me to use later for a frontend.

--- scraper.py
+++ scraper.py
@@ -1,13 +1,38 @@
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
-url = 'http://www.atpworldtour.com/en/scores/current'
+url = 'http://www.atpworldtour.com/en/scores/current/montpellier/375/draws'
 response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-table = soup.find(attrs={'class': 'scores-results-content'})
+table = soup.find(attrs={'class': 'scores-draw-table'})
 
-print table.prettify()
+for row in table.findAll('tr'):
+	print row.prettify()
+
+'''
+For Brandon:
+So the above for statment returns everything I need, but not in the base form.
+However, when I try to run the following:
+
+for row in table.findAll('tr'):
+	for cell in row.findAll('td'):
+		print cell.text
+
+It prints out the names, seeds, etc. but not the stuff within 'td' that is
+<img class
+
+This would be nice since it's the url for the flag of the player, and I could
+use that for my eventual front end.
+
+I suppose I could call those separately, but I'm not sure if there would be an
+easy way to match them up.
+
+Is there a way I can get that URL in line with everything else, to make it nice and neat?
+'''
+
+
+
 
 

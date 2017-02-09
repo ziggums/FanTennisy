8d43611113ba364ede9636a82794c9505d98ddbd
Ziggums <hc0109207@gmail.com>
2017-02-09 15:23:47 -0500

    Added functions for each grand slam

--- scraper.py
+++ scraper.py
@@ -5,10 +5,20 @@ import datetime
 # defining details for each tournament
 
 # Should I import datetime and automatically add year, so there's no required input?
-# Also, how much of this should I put in the functions, and how much of it should 
+
 def australian_open(year):
 	url = 'http://www.xscores.com/tennis/tournamentresults/atp-s/australian_open/' + str(year)
 	scrape(url)
+
+def us_open(year):
+	url = 'http://www.xscores.com/tennis/tournamentresults/atp-s/us_open/' + str(year)
+	scrape (url)
+
+def wimbledon(year):
+	url = 'http://www.xscores.com/tennis/tournamentresults/atp-s/wimbledon/' + str(year)
+
+def french_open(year):
+	url = 'http://www.xscores.com/tennis/tournamentresults/atp-s/wimbledon/' + str(year)
 
 # function for calling data
 def scrape(url):
@@ -27,4 +37,3 @@ now = datetime.datetime.now()
 year = now.year
 
 australian_open(year)
-

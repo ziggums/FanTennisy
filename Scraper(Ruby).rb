b2a57bcbf994064b8b32671ae3d261b2aad52269
Ziggums <hc0109207@gmail.com>
2017-02-17 17:53:22 -0500

    Updating Ruby scraper.

--- Scraper(Ruby).rb
+++ Scraper(Ruby).rb
@@ -4,3 +4,25 @@ require 'JSON'
 require 'Pry'
 require 'csv'
 
+def australian_open(year)
+	url = 'http://www.xscores.com/tennis/tournamentresults/atp-s/australian_open/' + year.to_s
+end
+
+def us_open(year)
+	url = 'http://www.xscores.com/tennis/tournamentresults/atp-s/us_open/' + year.to_s
+end
+
+def wimbledon(year)
+	url = 'http://www.xscores.com/tennis/tournamentresults/atp-s/wimbledon/' + year.to_s
+end
+
+def french_open(year)
+	url = 'http://www.xscores.com/tennis/tournamentresults/atp-s/french_open/' + year.to_s
+end
+
+def scrape(url)
+end
+
+page = HTTParty.get('http://www.xscores.com/tennis/tournamentresults/atp-s/australian_open/2017')
+
+Pry.start(binding)
